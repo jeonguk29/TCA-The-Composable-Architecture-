f65fa62a8a39275bad882470204376c44080e468
jeonguk29 <jeonguk29@naver.com>
2024-11-04 10:46:36 +0900

    feat : Child-to-parent communication

--- TCA_Presentation/TCA_Presentation/Feature/AddContactFeature.swift
+++ TCA_Presentation/TCA_Presentation/Feature/AddContactFeature.swift
@@ -11,7 +11,6 @@
 
 import ComposableArchitecture
 
-
 @Reducer
 struct AddContactFeature {
   @ObservableState
@@ -20,17 +19,29 @@ struct AddContactFeature {
   }
   enum Action {
     case cancelButtonTapped
+    case delegate(Delegate)
     case saveButtonTapped
     case setName(String)
+    enum Delegate {
+      // case cancel
+      case saveContact(Contact)
+    }
   }
+  @Dependency(\.dismiss) var dismiss
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
       case .cancelButtonTapped:
+        return .run { _ in await self.dismiss() }
+        
+      case .delegate:
         return .none
         
       case .saveButtonTapped:
-        return .none
+        return .run { [contact = state.contact] send in
+          await send(.delegate(.saveContact(contact)))
+          await self.dismiss()
+        }
         
       case let .setName(name):
         state.contact.name = name
